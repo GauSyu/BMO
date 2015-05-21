c62e57affacb1bbbcab0d55d9cd6ee69b2464fa0
Gau Syu <Gausyu@gmail.com>
2015-05-21 22:05:23 +0800

    Update the repertory

---
+++ makeglossaries BMO.bat
@@ -0,0 +1,4 @@
+makeindex -s BMO.ist -t BMO.glg -o BMO.gls BMO.glo
+makeindex -s BMO.ist -t BMO.slg -o BMO.sls BMO.slo
+makeindex -s BMO.ist -t BMO.cat-glg -o BMO.cat-gls BMO.cat-glo
+PAUSE
---
+++ makeglossaries.bat
@@ -0,0 +1,7 @@
+makeindex -s BMO.ist -t BMO.glg -o BMO.gls BMO.glo
+makeindex -s BMO.ist -t BMO.slg -o BMO.sls BMO.slo
+makeindex -s BMO.ist -t BMO.cat-glg -o BMO.cat-gls BMO.cat-glo
+makeindex -s Algebra.ist -t Algebra.glg -o Algebra.gls Algebra.glo
+makeindex -s Algebra.ist -t Algebra.slg -o Algebra.sls Algebra.slo
+makeindex -s Algebra.ist -t Algebra.cat-glg -o Algebra.cat-gls Algebra.cat-glo
+PAUSE
